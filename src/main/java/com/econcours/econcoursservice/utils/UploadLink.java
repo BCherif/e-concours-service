4de0dcd96b09d66af4fbaea4f966dfc8997399b4
mlkomou <malifoli.musique@gmail.com>
2023-04-16 09:09:10 +0000

    last upd

--- src/main/java/com/econcours/econcoursservice/utils/UploadLink.java
+++ src/main/java/com/econcours/econcoursservice/utils/UploadLink.java
@@ -1,6 +1,6 @@
 package com.econcours.econcoursservice.utils;
 
 public class UploadLink {
-    //public static final String ECONCOURS_LINK = "/Volumes/Data/free/E-CONCOURS/files";
-    public static final String ECONCOURS_LINK = "/Users/cherif/Uploads/ECONCOURS";
+    public static final String ECONCOURS_LINK = "/Volumes/Data/free/E-CONCOURS/files";
+//    public static final String ECONCOURS_LINK = "/Users/cherif/Uploads/ECONCOURS";
 }
